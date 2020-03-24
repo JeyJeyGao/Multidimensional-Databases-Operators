ac6fa8c304b27393c82ba5ddffc9ed976bd039fe
Junjie <junjiega@usc.edu>
2020-03-23 18:49:37 -0700

    connection succeed

---
+++ cube.py
@@ -0,0 +1,27 @@
+class Cube:
+    def __init__(self):
+        self.original_data = None
+        self.data_cube = None
+        self.dimentions = None
+        self.dimention_value = None
+        self.elements = None
+    
+    def data_to_cube():
+        pass
+
+    def cube_to_data():
+        pass
+
+    def pull():
+        pass
+
+    def push():
+        pass
+
+    def destroy():
+        pass
+
+    def restriction():
+        pass
+    
+    
